8f44685c7bb1c0ab3e3fa35de17b1ddcf81a2b70
ajay dharajiya <dharajiyaajay575@gmail.com>
2026-09-07 13:26:50 +0000

    final v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yonolootzone-v6';
+const CACHE='yonolootzone-v10';
 const CORE=['./','./index.html','./assets/style.css','./assets/app.js','./assets/all-yono-header.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
